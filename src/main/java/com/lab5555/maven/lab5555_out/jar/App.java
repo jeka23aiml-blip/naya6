b0c53e01d7b3ff51c50c089f3f88cf8268cd2b11
jeka23aiml-blip <jeka23aiml@cmrit.ac.in>
2026-06-20 21:19:43 +0530

    Add files via upload

---
+++ src/main/java/com/lab5555/maven/lab5555_out/jar/App.java
@@ -0,0 +1,13 @@
+package com.lab5555.maven.lab5555_out.jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
